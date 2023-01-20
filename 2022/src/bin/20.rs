6abbba083e453726c00ec70c960ef71d1a13847f
Sergey Adamovich <adamovichserge@gmail.com>
2023-01-20 14:17:18 +0100

    Add 2022.20 solution [part 2]

--- 2022/src/bin/20.rs
+++ 2022/src/bin/20.rs
@@ -3,23 +3,29 @@ use aoc::read_file_input;
 fn main() {
     let input = read_file_input("20.txt".to_string());
 
-    let mut numbers = vec![];
+    let mut numbers: Vec<isize> = vec![];
 
     for line in input.lines() {
         numbers.push(line.parse::<isize>().unwrap());
     }
 
-    println!("{}", mix(&numbers));
+    println!("{}", mix(&numbers, 1));
+
+    let numbers = numbers.iter().map(|x| x * 811589153).collect::<Vec<_>>();
+
+    println!("{}", mix(&numbers, 10));
 }
 
-fn mix(numbers: &[isize]) -> isize {
+fn mix(numbers: &[isize], iterations: u8) -> isize {
     let mut res = (0..numbers.len()).collect::<Vec<usize>>();
 
-    for (i, &n) in numbers.iter().enumerate() {
-        let pos = res.iter().position(|&y| y == i).unwrap();
-        res.remove(pos);
-        let new_index = (pos as isize + n).rem_euclid(res.len() as isize) as usize;
-        res.insert(new_index, i);
+    for _ in 0..iterations {
+        for (i, &n) in numbers.iter().enumerate() {
+            let pos = res.iter().position(|&y| y == i).unwrap();
+            res.remove(pos);
+            let new_index = (pos as isize + n).rem_euclid(res.len() as isize) as usize;
+            res.insert(new_index, i);
+        }
     }
 
     let zero_index = numbers.iter().position(|&i| i == 0).unwrap();
